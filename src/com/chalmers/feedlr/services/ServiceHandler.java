6c9c87aab3740ee9f652f448cfaf59016fc6f7cd
Olle Werme <olle.werme@gmail.com>
2012-09-29 21:25:21 +0200

    Simple changes to support multiple services

--- src/com/chalmers/feedlr/services/ServiceHandler.java
+++ src/com/chalmers/feedlr/services/ServiceHandler.java
@@ -2,28 +2,49 @@ package com.chalmers.feedlr.services;
 
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.chalmers.feedlr.listeners.AuthListener;
-import com.chalmers.feedlr.twitter.TwitterHelper;
+import com.chalmers.feedlr.twitter.TwitterAuthHelper;
+import com.chalmers.feedlr.util.Services;
 
 public class ServiceHandler {
-	
+		
 	private Context context;
 	
 	private TwitterAuthListener twitterAuthListener = new TwitterAuthListener();
 	
-	private TwitterHelper twitter;
+	private TwitterAuthHelper twitterAuthHelper;
 
 	public ServiceHandler(Context context) {
 		this.context = context;
 		
-		twitter = new TwitterHelper(context);
-		twitter.setAuthListener(twitterAuthListener);
+		twitterAuthHelper = new TwitterAuthHelper(context);	
+		// For every service...
 	}
 	
-	public void authorizeTwitter() {
-		twitter.authorize();
+	public void authorize(int service) {
+		switch (service) {
+			case Services.TWITTER: 
+				twitterAuthHelper.authorize(twitterAuthListener); break;
+	//		case Service.FACEBOOK:
+	//			facebookAuthHelper.authorize(); break;
+			default: 
+				Log.w(getClass().getName(), "Unknown service");
+		}
+	}
+	
+	public boolean isAuthorized(int service) {
+		switch (service) {
+			case Services.TWITTER: 
+				return twitterAuthHelper.isAuthorized();
+//			case Service.FACEBOOK:
+//				return facebookAuthHelper.isAuthorized();
+			default: 
+				Log.w(getClass().getName(), "Unknown service");
+		}
+		return false;
 	}
 
 	private class TwitterAuthListener implements AuthListener {
@@ -42,6 +63,6 @@ public class ServiceHandler {
 	}
 
 	public void onTwitterAuthCallback(Intent data) {
-		twitter.onAuthCallback(data);
+		twitterAuthHelper.onAuthCallback(data);
 	}
 }
