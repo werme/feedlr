c398432e483322eb5ca35b6a44f1f75b9df85753
Olle Werme <olle.werme@gmail.com>
2012-10-03 00:29:00 +0200

    Added constructor to User model

--- src/com/chalmers/feedlr/model/User.java
+++ src/com/chalmers/feedlr/model/User.java
@@ -10,19 +10,33 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class User {
 	private String userName, profileImageURL;
-	
+
+	public User() {
+	}
+
+	public User(String userName) {
+		this.userName = userName;
+	}
+
+	public User(String userName, String profileImageURL) {
+		this.userName = userName;
+		this.profileImageURL = profileImageURL;
+	}
+
 	@JsonProperty("name")
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+
 	@JsonProperty("profile_image_url")
 	public void setProfileImageURL(String profileImageURL) {
 		this.profileImageURL = profileImageURL;
 	}
-	
+
 	public String getUserName() {
 		return userName;
 	}
+
 	public String getProfileImageURL() {
 		return profileImageURL;
 	}
